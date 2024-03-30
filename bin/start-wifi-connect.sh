3fbc470af774549d1615300935776c7fd404d415
iory <ab.ioryz@gmail.com>
2024-03-31 01:00:19 +0900

    Add sleep time to ensure that any network services have enough time to initialize before checking for a default gateway.

--- bin/start-wifi-connect.sh
+++ bin/start-wifi-connect.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Wait for 30 seconds to ensure that any network services have enough time to initialize before checking for a default gateway.
+# This can be particularly useful in environments where network connections take a moment to establish after boot.
+sleep 30
+
 # Check for a default gateway
 if ip route | grep -q default; then
     printf 'Default gateway found. Skipping WiFi Connect.\n'
