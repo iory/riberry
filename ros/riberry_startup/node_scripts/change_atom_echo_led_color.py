26c6407f19da01f5046b97d470778a9590601f18
Iori Yanokura <ab.ioryz@gmail.com>
2024-11-06 02:17:11 +0900

    Lock atom echo led

--- ros/riberry_startup/node_scripts/change_atom_echo_led_color.py
+++ ros/riberry_startup/node_scripts/change_atom_echo_led_color.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 
+from filelock import FileLock
+from filelock import Timeout
 from i2c_for_esp32 import WirePacker
 import rospy
 from std_msgs.msg import ColorRGBA
@@ -10,7 +12,10 @@ from riberry.i2c_base import I2C
 if __name__ == "__main__":
     rospy.init_node("change_atom_echo_led_color")
 
-    i2c = I2C(0x41, bus=3)
+    bus = 3
+    i2c = I2C(0x41, bus=bus)
+    lock_path = f"/tmp/i2c-{bus}.lock"
+    lock = FileLock(lock_path, timeout=10)
 
     def callback(msg):
         r, g, b = int(msg.r), int(msg.g), int(msg.b)
@@ -26,7 +31,23 @@ if __name__ == "__main__":
             packer.write(ord(s))
         packer.end()
         if packer.available():
-            i2c.write(packer.buffer[: packer.available()])
+            try:
+                lock.acquire()
+            except Timeout as e:
+                rospy.logerr(str(e))
+                return
+            try:
+                i2c.write(packer.buffer[: packer.available()])
+            except OSError as e:
+                rospy.logerr(str(e))
+            except TimeoutError as e:
+                rospy.logerr(f"I2C Write error {e}")
+            finally:
+                try:
+                    lock.release()
+                except Timeout as e:
+                    rospy.logerr(str(e))
+
 
     sub = rospy.Subscriber("color_rgb", ColorRGBA, queue_size=1, callback=callback)
     rospy.loginfo('Change Atom echo led color node start.')
