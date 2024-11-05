402bb951e6edcae5ecea63cc65585413806aade3
HarukiKozukapenguin <kozukozupenguin@g.ecc.u-tokyo.ac.jp>
2024-11-05 18:02:12 +0900

    [firmware][atom_s3_battery_display] include variable and function in GUI system

--- firmware/atom_s3_battery_display/lib/display_battery_mode1/display_battery_mode1.h
+++ firmware/atom_s3_battery_display/lib/display_battery_mode1/display_battery_mode1.h
@@ -5,17 +5,26 @@
 #include <atom_s3_lcd.h>
 #include <atom_s3_i2c.h>
 
-class DisplayBatteryMode1 : public Mode {
+// voltage unit: V
+class DisplayBatteryMode1 : public Mode
+{
 public:
   DisplayBatteryMode1(AtomS3LCD &lcd, AtomS3I2C &i2c);
   void createTask(uint8_t xCoreID) override;
 
+public:
+  void setVoltageRange(float newMaxVoltage, float newMinVoltage);
+  void displayFrame();
+  void updateVoltage(float voltage);
+
 private:
+  float maxVoltage_;
+  float minVoltage_;
+  float Voltage_;
+
   static DisplayBatteryMode1* instance; /**< Singleton instance of DisplayInformationMode. */
   AtomS3LCD &atoms3lcd;
   AtomS3I2C &atoms3i2c;
-
   static void task(void *parameter);
 };
-
 #endif // DISPLAY_INFORMATION_MODE_H
