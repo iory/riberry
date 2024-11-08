6f1aa2776f8541162fc90ccfb5eed4554976fb08
HarukiKozukapenguin <kozukozupenguin@g.ecc.u-tokyo.ac.jp>
2024-11-08 16:07:01 +0900

    [ros][display_battery_mode] subscriber for voltage status to send voltage value

--- ros/riberry_startup/node_scripts/display_battery_mode.py
+++ ros/riberry_startup/node_scripts/display_battery_mode.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import rospy
-from std_msgs.msg import Int32
+from std_msgs.msg import Int32, Float32
 from std_msgs.msg import String
 
 from riberry.i2c_base import I2CBase
@@ -16,20 +16,18 @@ class DisplayBatteryMode(I2CBase):
         #     "/i2cbutton_state", Int32, callback=self.button_cb, queue_size=1
         # )
         rospy.Subscriber("/i2c_mode", String, callback=self.mode_cb, queue_size=1)
+        rospy.Subscriber("voltage_status", Float32, callback=self.voltage_cb, queue_size=1)
+        self.voltage = 0.0
 
         rospy.Timer(rospy.Duration(0.1), self.timer_callback)
 
-    # def button_cb(self, msg):
-    #     """
-    #     When AtomS3 is ServoControlMode and single-click pressed,
-    #     toggle servo control.
-    #     """
-    #     if self.mode == "ServoControlMode" and msg.data == 1:
-    #         rospy.loginfo(
-    #             "AtomS3 is ServoControlMode and single-click-pressed."
-    #             + " Toggle servo control."
-    #         )
-    #         self.toggle_servo_on_off()
+    def voltage_cb(self, msg):
+        """
+        When AtomS3 is ServoControlMode and single-click pressed,
+        toggle servo control.
+        """
+        if self.mode == "DisplayBatteryMode1":
+            self.voltage = msg.data
 
     def mode_cb(self, msg):
         """
@@ -44,7 +42,7 @@ class DisplayBatteryMode(I2CBase):
         # sent_str = "Servo control mode\n\nSingle Click:\nServo on off"
         # self.send_string(sent_str)
         if self.mode=="DisplayBatteryMode1":
-            self.send_string("12.0")
+            self.send_string(str(self.voltage))
         elif self.mode=="DisplayBatteryMode2":
             self.send_string("fuga")
 
