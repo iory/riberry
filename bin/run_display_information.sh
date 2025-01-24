e92264941757f005d4aba8b7506316044268db40
Naoya Yamaguchi <708yamaguchi@gmail.com>
2025-01-24 18:25:11 +0900

    Change ROS_MASTER_URI of run_display_information by systemctl command line

--- bin/run_display_information.sh
+++ bin/run_display_information.sh
@@ -6,4 +6,10 @@ elif [ -f /opt/ros/noetic/setup.bash ]; then
     . /opt/ros/noetic/setup.bash
 fi
 
+# Export ROS_MASTER_URI after /opt/ros/.../setup.bash
+# because ROS_MASTER_URI is set to localhost by the script.
+if [ -n "$RIBERRY_ROS_MASTER_IP" ]; then
+    export ROS_MASTER_URI=http://$RIBERRY_ROS_MASTER_IP:11311
+fi
+
 /usr/bin/python3 /usr/local/bin/display_information.py
