4c2280847cfb9908738ddcfa3d8a0bc01f41a328
Iori Yanokura <ab.ioryz@gmail.com>
2025-02-21 23:40:33 +0900

    [esp_now_pairing] Refactor Pairing class

--- firmware/esp_now_pairing/lib/pairing/pairing.h
+++ firmware/esp_now_pairing/lib/pairing/pairing.h
@@ -17,125 +17,62 @@ struct PairingData {
 class Pairing {
 public:
     Pairing();
+
     bool setupESPNOW();
+    void waitForESPNOWSetup();
+
     void sendPairingData(const PairingData& data);
-    void setupBroadcastPeer();
     static bool receivePairingData(String macString, PairingData& receivedData);
     void broadcastMACAddress();
+    static bool addPeer(const String& macAddress);
+    void checkPendingPeers();
+
     String getMyMACAddress() const;
     String getStatus() const;
     void reset();
+
     static bool isPairingActive() { return _pairingActive; }
-    static void stopPairing() {
-        mutex_.lock();
-        _pairingActive = false;
-        mutex_.unlock();
-    }
-    static void startPairing() {
-        mutex_.lock();
-        _pairingActive = true;
-        mutex_.unlock();
-    }
+    static void startPairing();
+    static void stopPairing();
     bool isPaired() const { return pairedMACAddresses.size() > 0; }
     std::vector<String> getPairedMACAddresses() const { return pairedMACAddresses; }
     std::map<String, PairingData> getPairedData() const { return pairingDataMap; }
-    static bool addPeer(const String& macAddress);
-    void checkPendingPeers();
-    void createTask(uint8_t xCoreID) {
-        if (taskHandle == nullptr) {
-            xTaskCreatePinnedToCore([](void* _this) { static_cast<Pairing*>(_this)->task(); },
-                                    "Pairing Task", 4096, this, 1, &taskHandle, xCoreID);
-        }
-    }
 
-    virtual void resumeTask(uint8_t xCoreID) {
-        if (taskHandle == nullptr) {
-            createTask(xCoreID);
-        } else if (taskHandle != nullptr) {
-            vTaskResume(taskHandle);
-        }
-        suspend = false;
-    }
+    void setupBroadcastPeer();
+    void createTask(uint8_t xCoreID);
+    void task();
+    void handleSuspend(bool* resumeAfterSuspend);
+    void resumeTask(uint8_t xCoreID);
+    void suspendTask();
+    void deleteTask();
 
-    void stopBackgroundTask() {
-        if (taskHandle != nullptr) {
-            vTaskSuspend(taskHandle);
-        }
-    }
-
-    void suspendTask() {
-        if (taskHandle != nullptr) {
-            suspend = true;
-            delay(100);
-            vTaskSuspend(taskHandle);
-        }
-    }
-
-    void deleteTask() {
-        if (taskHandle != nullptr) {
-            TaskHandle_t taskToDelete = taskHandle;
-            vTaskDelete(taskToDelete);
-            while (eTaskGetState(taskToDelete) != eDeleted) {
-                vTaskDelay(1 / portTICK_PERIOD_MS);
-            }
-            taskHandle = nullptr;
-        }
-    }
-    void resumeBackgroundTask() {
-        if (taskHandle != nullptr) {
-            vTaskResume(taskHandle);
-        }
-    }
-
-    void setDataToSend(const PairingData& data) {
-        dataToSend = data;
-        dataToSendInitialized = true;
-    }
+    void setDataToSend(const PairingData& data);
 
 private:
     static MutexHelper mutex_;
+
+    // EspNow callbacks
     static void onDataSent(const uint8_t* mac_addr, esp_now_send_status_t status);
     static void onDataRecv(const uint8_t* mac_addr, const uint8_t* data, int data_len);
+
+    // Mac addresses and Pairing Information
     uint8_t myMACAddress[6];
-    const uint8_t broadcastAddress[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-    esp_now_peer_info_t peerInfo;
-    static String statusStr;
     static std::map<String, PairingData> pairingDataMap;
     static std::map<String, unsigned long> pendingPeers;
     static std::vector<String> pairedMACAddresses;
-    const unsigned long pairingTimeout = 5000;
-    PairingData dataToSend;
-    bool dataToSendInitialized = false;
-    static bool _pairingActive;
+    const uint8_t broadcastAddress[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
+    // Task related variables
     TaskHandle_t taskHandle = nullptr;
     bool suspend = false;
 
-    void task() {
-        while (!setupESPNOW()) {
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-        }
-        bool resumeAfterSuspend = false;
-        for (;;) {
-            if (suspend) {
-                resumeAfterSuspend = true;
-                vTaskDelay(100 / portTICK_PERIOD_MS);
-                continue;
-            } else if (resumeAfterSuspend) {
-                resumeAfterSuspend = false;
-                while (!setupESPNOW()) {
-                    vTaskDelay(1000 / portTICK_PERIOD_MS);
-                }
-            }
-            checkPendingPeers();
-            if (_pairingActive) {
-                broadcastMACAddress();
-            }
-            if (isPaired() && dataToSendInitialized) {
-                sendPairingData(dataToSend);
-            }
-            vTaskDelay(100 / portTICK_PERIOD_MS);
-        }
-    }
+    // status
+    static String statusStr;
+    static bool _pairingActive;
+    const unsigned long pairingTimeout = 5000;
+
+    PairingData dataToSend;
+    bool dataToSendInitialized = false;
 };
 
 #endif  // PAIRING_H
