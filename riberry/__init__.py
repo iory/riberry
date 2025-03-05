b0ad3e1cdce050d4e143abc8ca1afe5a05e9a7ef
Iori Yanokura <ab.ioryz@gmail.com>
2025-03-05 21:34:42 +0900

    Fixed riberry version for python

--- riberry/__init__.py
+++ riberry/__init__.py
@@ -3,6 +3,6 @@
 import pkg_resources
 
 try:
-    version = pkg_resources.get_distribution("riberry-robot").version
+    __version__ = pkg_resources.get_distribution("riberry-robot").version
 except pkg_resources.DistributionNotFound:
     print("riberry-robot is not installed")
