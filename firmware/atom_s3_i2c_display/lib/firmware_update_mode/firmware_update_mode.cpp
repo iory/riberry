aecdbbe7b753966f61e4256e474b39cb70e2568a
Iori Yanokura <ab.ioryz@gmail.com>
2025-03-07 17:02:00 +0900

    [atoms3] Skip Firmware update mode

--- firmware/atom_s3_i2c_display/lib/firmware_update_mode/firmware_update_mode.cpp
+++ firmware/atom_s3_i2c_display/lib/firmware_update_mode/firmware_update_mode.cpp
@@ -1,6 +1,6 @@
 #include <firmware_update_mode.h>
 
-FirmwareUpdateMode::FirmwareUpdateMode() : Mode(ModeType::FIRMWARE_UPDATE) {}
+FirmwareUpdateMode::FirmwareUpdateMode() : Mode(ModeType::FIRMWARE_UPDATE, true) {}
 
 void FirmwareUpdateMode::task(PrimitiveLCD &lcd, CommunicationBase &com) {
     while (running) {
