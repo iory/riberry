a39078f22c71f60f160cb60cb58f049291caf9b7
Naoya Yamaguchi <708yamaguchi@gmail.com>
2025-09-16 18:33:05 +0900

    Fixes to pass CI

--- firmware/atomic_robot_tools/src/dynamixel_extended_position.cpp
+++ firmware/atomic_robot_tools/src/dynamixel_extended_position.cpp
@@ -2,7 +2,6 @@
 #include <Dynamixel2Arduino.h>
 #include <M5AtomS3.h>
 
-
 // Dynamixel2Arduino ////////////////////////////////////////////////////////
 #define DXL_SERIAL Serial1
 #define SERIAL_CONFIG SERIAL_8N1
@@ -10,7 +9,7 @@
 using namespace ControlTableItem;
 
 const long BAUDRATE = 57600;
-const int TIMEOUT = 100;  //ms
+const int TIMEOUT = 100;  // ms
 const uint8_t DXL_ID = 1;
 const float DXL_PROTOCOL_VERSION = 2.0;
 
@@ -20,58 +19,54 @@ const int TX_PIN = 38;
 
 Dynamixel2Arduino dxl(DXL_SERIAL, EN_PIN);
 
-void setupDXL()
-{
-  DXL_SERIAL.begin(BAUDRATE, SERIAL_CONFIG, RX_PIN, TX_PIN, false, TIMEOUT);
-  dxl.begin(BAUDRATE);
-  dxl.setPortProtocolVersion(DXL_PROTOCOL_VERSION);
+void setupDXL() {
+    DXL_SERIAL.begin(BAUDRATE, SERIAL_CONFIG, RX_PIN, TX_PIN, false, TIMEOUT);
+    dxl.begin(BAUDRATE);
+    dxl.setPortProtocolVersion(DXL_PROTOCOL_VERSION);
 
-  // 定格以上の電圧をかけるとShutdownがかかってしまうので、ここで無理やりビットフラグを0にする
-  dxl.writeControlTableItem(ControlTableItem::SHUTDOWN, DXL_ID, 0);
+    // 定格以上の電圧をかけるとShutdownがかかってしまうので、ここで無理やりビットフラグを0にする
+    dxl.writeControlTableItem(ControlTableItem::SHUTDOWN, DXL_ID, 0);
 
-  dxl.torqueOff(DXL_ID);
-  dxl.setOperatingMode(DXL_ID, OP_EXTENDED_POSITION);
-  dxl.torqueOn(DXL_ID);
+    dxl.torqueOff(DXL_ID);
+    dxl.setOperatingMode(DXL_ID, OP_EXTENDED_POSITION);
+    dxl.torqueOn(DXL_ID);
 
-  dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_ID, 0);
+    dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_ID, 0);
 }
 /////////////////////////////////////////////////////////////////////////////
 
-
 void setup() {
-  M5.begin();
-  M5.Lcd.setRotation(0);  // 画面向き設定（USB位置基準 0：下/ 1：右/ 2：上/ 3：左）
-  M5.Lcd.setTextSize(2);  // 文字サイズ（整数倍率）
+    M5.begin();
+    M5.Lcd.setRotation(0);  // 画面向き設定（USB位置基準 0：下/ 1：右/ 2：上/ 3：左）
+    M5.Lcd.setTextSize(2);  // 文字サイズ（整数倍率）
 
-  setupDXL();
+    setupDXL();
 
-  uint16_t read_voltage_limit = dxl.readControlTableItem(ControlTableItem::MAX_VOLTAGE_LIMIT, DXL_ID);
-  M5.Lcd.clear();
-  M5.Lcd.setCursor(0, 0);
-  M5.Lcd.print(read_voltage_limit);
+    uint16_t read_voltage_limit =
+            dxl.readControlTableItem(ControlTableItem::MAX_VOLTAGE_LIMIT, DXL_ID);
+    M5.Lcd.clear();
+    M5.Lcd.setCursor(0, 0);
+    M5.Lcd.print(read_voltage_limit);
 
-  delay(2000);
+    delay(2000);
 }
 
-
 void loop() {
-  dxl.setGoalPosition(DXL_ID, 1080, UNIT_DEGREE);
-  for (byte i = 0; i < 50; i++)
-  {
-    float present_deg = dxl.getPresentPosition(DXL_ID, UNIT_DEGREE);
-    M5.Lcd.clear();
-    M5.Lcd.setCursor(0, 0);
-    M5.Lcd.print(present_deg);
-    delay(100);
-  }
+    dxl.setGoalPosition(DXL_ID, 1080, UNIT_DEGREE);
+    for (byte i = 0; i < 50; i++) {
+        float present_deg = dxl.getPresentPosition(DXL_ID, UNIT_DEGREE);
+        M5.Lcd.clear();
+        M5.Lcd.setCursor(0, 0);
+        M5.Lcd.print(present_deg);
+        delay(100);
+    }
 
-  dxl.setGoalPosition(DXL_ID, 0, UNIT_DEGREE);
-  for (byte i = 0; i < 50; i++)
-  {
-    float present_deg = dxl.getPresentPosition(DXL_ID, UNIT_DEGREE);
-    M5.Lcd.clear();
-    M5.Lcd.setCursor(0, 0);
-    M5.Lcd.print(present_deg);
-    delay(100);
-  }
+    dxl.setGoalPosition(DXL_ID, 0, UNIT_DEGREE);
+    for (byte i = 0; i < 50; i++) {
+        float present_deg = dxl.getPresentPosition(DXL_ID, UNIT_DEGREE);
+        M5.Lcd.clear();
+        M5.Lcd.setCursor(0, 0);
+        M5.Lcd.print(present_deg);
+        delay(100);
+    }
 }
--- firmware/atomic_robot_tools/src/dynamixel_hold_free.cpp
+++ firmware/atomic_robot_tools/src/dynamixel_hold_free.cpp
@@ -2,7 +2,6 @@
 #include <Dynamixel2Arduino.h>
 #include <M5AtomS3.h>
 
-
 // Dynamixel2Arduino ////////////////////////////////////////////////////////
 #define DXL_SERIAL Serial1
 #define SERIAL_CONFIG SERIAL_8N1
@@ -10,7 +9,7 @@
 using namespace ControlTableItem;
 
 const long BAUDRATE = 57600;
-const int TIMEOUT = 100;  //ms
+const int TIMEOUT = 100;  // ms
 const uint8_t DXL_ID = 1;
 const float DXL_PROTOCOL_VERSION = 2.0;
 
@@ -20,55 +19,61 @@ const int TX_PIN = 38;
 
 Dynamixel2Arduino dxl(DXL_SERIAL, EN_PIN);
 
-void setupDXL()
-{
-  DXL_SERIAL.begin(BAUDRATE, SERIAL_CONFIG, RX_PIN, TX_PIN, false, TIMEOUT);
-  dxl.begin(BAUDRATE);
-  dxl.setPortProtocolVersion(DXL_PROTOCOL_VERSION);
+void setupDXL() {
+    DXL_SERIAL.begin(BAUDRATE, SERIAL_CONFIG, RX_PIN, TX_PIN, false, TIMEOUT);
+    dxl.begin(BAUDRATE);
+    dxl.setPortProtocolVersion(DXL_PROTOCOL_VERSION);
 
-  dxl.torqueOff(DXL_ID);
-  dxl.setOperatingMode(DXL_ID, OP_EXTENDED_POSITION);
-  dxl.torqueOn(DXL_ID);
+    dxl.torqueOff(DXL_ID);
+    dxl.setOperatingMode(DXL_ID, OP_EXTENDED_POSITION);
+    dxl.torqueOn(DXL_ID);
 
-  dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_ID, 0);
+    dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_ID, 0);
 }
 /////////////////////////////////////////////////////////////////////////////
 
-
 void setup() {
-  M5.begin();
-  M5.Lcd.setRotation(0);  // 画面向き設定（USB位置基準 0：下/ 1：右/ 2：上/ 3：左）
-  M5.Lcd.setTextSize(2);  // 文字サイズ（整数倍率）
+    M5.begin();
+    M5.Lcd.setRotation(0);  // 画面向き設定（USB位置基準 0：下/ 1：右/ 2：上/ 3：左）
+    M5.Lcd.setTextSize(2);  // 文字サイズ（整数倍率）
 
-  setupDXL();
+    setupDXL();
 
-  M5.Lcd.clear();
-  M5.Lcd.setCursor(0, 0);
-  M5.Lcd.print("Press Button to HOLD");
-  do { M5.update(); delay(10); } while (!M5.BtnA.isPressed());
+    M5.Lcd.clear();
+    M5.Lcd.setCursor(0, 0);
+    M5.Lcd.print("Press Button to HOLD");
+    do {
+        M5.update();
+        delay(10);
+    } while (!M5.BtnA.isPressed());
 
-  delay(1000);
+    delay(1000);
 }
 
-
 void loop() {
-  // Hold
-  dxl.torqueOn(DXL_ID);
-  float present_pos = dxl.getPresentPosition(DXL_ID);
-  dxl.setGoalPosition(DXL_ID, present_pos);
-  M5.Lcd.clear();
-  M5.Lcd.setCursor(0, 0);
-  M5.Lcd.print("Press Button to FREE");
-  do { M5.update(); delay(10); } while (!M5.BtnA.isPressed());
+    // Hold
+    dxl.torqueOn(DXL_ID);
+    float present_pos = dxl.getPresentPosition(DXL_ID);
+    dxl.setGoalPosition(DXL_ID, present_pos);
+    M5.Lcd.clear();
+    M5.Lcd.setCursor(0, 0);
+    M5.Lcd.print("Press Button to FREE");
+    do {
+        M5.update();
+        delay(10);
+    } while (!M5.BtnA.isPressed());
 
-  delay(1000);
+    delay(1000);
 
-  // Free
-  dxl.torqueOff(DXL_ID);
-  M5.Lcd.clear();
-  M5.Lcd.setCursor(0, 0);
-  M5.Lcd.print("Press Button to HOLD");
-  do { M5.update(); delay(10); } while (!M5.BtnA.isPressed());
+    // Free
+    dxl.torqueOff(DXL_ID);
+    M5.Lcd.clear();
+    M5.Lcd.setCursor(0, 0);
+    M5.Lcd.print("Press Button to HOLD");
+    do {
+        M5.update();
+        delay(10);
+    } while (!M5.BtnA.isPressed());
 
-  delay(1000);
+    delay(1000);
 }
